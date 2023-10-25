a939e3e31bbebc8a858ee7875b81416958f3c45b
Kodesha Hasindu <80147754+KodeshaHasindu@users.noreply.github.com>
2023-10-25 14:13:53 +0530

    therd commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let spaces = "     "; 
-    let spaces = spaces.len();
-
-    println!("{spaces}");
+    // addition
+    let sum = 5 + 10;
+    println!("{sum}");
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("{difference}");
+    // multiplication
+    let product = 4 * 30;
+    println!("{product}");
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+    println!("{quotient}{truncated}");
+    // remainder
+    let remainder = 43 % 5;
+    println!("{remainder}");
+    
 }
